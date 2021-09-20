110c2da43271bf13120596a2837a01e3a47a06c0
BenFrancka <benfrancka@gmail.com>
2021-09-20 09:16:21 -0700

    adds test for post route, test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,28 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('inventory rouotes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates an inventory item using POST', async () => {
+    const potatoes = {
+      itemName: 'potatoes',
+      category: 'produce',
+      itemPrice: 7,
+      totalItems: 3,
+      totalPrice: 21,
+    };
+    const res = await request(app).post('/api/v1/inventory').send(potatoes);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...potatoes,
+    });
   });
 });
