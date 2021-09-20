60571148ae444cd1cd7c553d455142e246507e6f
BenFrancka <benfrancka@gmail.com>
2021-09-20 09:19:59 -0700

    adds test for get by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Inventory from '../lib/models/Inventory.js';
 
 describe('inventory rouotes', () => {
   beforeEach(() => {
@@ -26,5 +27,18 @@ describe('inventory rouotes', () => {
       id: '1',
       ...potatoes,
     });
+  });
+
+  it('gets an inventory item by id using GET', async () => {
+    const potatoes = await Inventory.insert({
+      itemName: 'potatoes',
+      category: 'produce',
+      itemPrice: 7,
+      totalItems: 3,
+      totalPrice: 21,
+    });
+    const res = await request(app).get(`/api/v1/inventory/${potatoes.id}`);
+
+    expect(res.body).toEqual(potatoes);
   });
 });
