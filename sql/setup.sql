2916338c6db1ff817eaf6c508adbf9d2d3fb6cc5
BenFrancka <benfrancka@gmail.com>
2021-09-20 08:49:01 -0700

    adds table setup to setup sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,10 @@
 DROP TABLE IF EXISTS inventory;
 
-CREATE TABLE inventory ();
+CREATE TABLE inventory (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    item_name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    item_price INTEGER NOT NULL,
+    total_items INTEGER NOT NULL,
+    total_price INTEGER NOT NULL
+);
